f77def434553638de4f5ce940415c6475763340a
iAtomPlaza <stevecoulter99@gmail.com>
2021-06-29 15:07:00 -0700

    setup basic component rendering

---
+++ component.go
@@ -0,0 +1,5 @@
+package gerbil
+
+type Component interface {
+	Render() string
+}
--- gerbil.go
+++ gerbil.go
@@ -1,8 +1,34 @@
 package gerbil
 
+import (
+	"fmt"
+	"reflect"
+	"syscall/js"
+)
+
+var App *Gerbil
+
 type Gerbil struct {
 }
 
 func New() *Gerbil {
 	return &Gerbil{}
+}
+
+func (g *Gerbil) RenderComponent(c interface{}) {
+
+	iface := reflect.TypeOf((*Component)(nil)).Elem()
+	component := reflect.ValueOf(c)
+	if component.Kind() != reflect.Ptr {
+		fmt.Printf("'%s' must be a pointer to a struct that implements the '*gerbil.Component' interface\n", component.Type())
+		return
+	}
+
+	if !component.Type().Implements(iface) {
+		fmt.Printf("error: component %v does not implement %v\n", component.Type(), iface)
+	}
+
+	dom := js.Global().Get("document")
+	root := dom.Call("getElementById", "root")
+	root.Set("innerHTML", c.(Component).Render())
 }
