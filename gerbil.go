52cf3849a13ad1c2e3d30b3ddd02c8aa297f3061
iAtomPlaza <stevecoulter99@gmail.com>
2021-06-30 12:42:38 -0700

    implement basic routing

--- gerbil.go
+++ gerbil.go
@@ -1,34 +1,56 @@
 package gerbil
 
 import (
-	"fmt"
-	"reflect"
+	"strings"
 	"syscall/js"
 )
 
 var App *Gerbil
 
 type Gerbil struct {
+	routes map[string]interface{}
 }
 
 func New() *Gerbil {
-	return &Gerbil{}
+	return &Gerbil{
+		routes: make(map[string]interface{}),
+	}
 }
 
-func (g *Gerbil) RenderComponent(c interface{}) {
-
-	iface := reflect.TypeOf((*Component)(nil)).Elem()
-	component := reflect.ValueOf(c)
-	if component.Kind() != reflect.Ptr {
-		fmt.Printf("'%s' must be a pointer to a struct that implements the '*gerbil.Component' interface\n", component.Type())
-		return
-	}
-
-	if !component.Type().Implements(iface) {
-		fmt.Printf("error: component %v does not implement %v\n", component.Type(), iface)
-	}
-
+func (*Gerbil) renderApplication(c interface{}) {
 	dom := js.Global().Get("document")
 	root := dom.Call("getElementById", "root")
 	root.Set("innerHTML", c.(Component).Render())
+}
+
+func (*Gerbil) currentRoute() string {
+	window := js.Global().Get("window")
+	location := window.Get("location")
+	rawUrl := location.Get("href").String()
+	baseUrl := strings.Split(rawUrl, "://")[1]
+	if len(strings.Split(baseUrl, "/?")) == 1 {
+		return "/"
+	}
+	if strings.Split(baseUrl, "/?")[1] == "" {
+		return "/"
+	}
+	return "/" + strings.Split(baseUrl, "/?")[1]
+}
+
+func (g *Gerbil) Run() {
+	component, ok := g.routes[g.currentRoute()]
+	if ok {
+		g.renderApplication(component)
+	} else {
+		//TODO - render 404
+	}
+
+	<-make(chan struct{})
+}
+
+func (g *Gerbil) Route(path string, component interface{}) {
+	if !IsComponent(component) {
+		return
+	}
+	g.routes[path] = component
 }
