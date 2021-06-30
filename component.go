8a52becb740df9e0d135711b720adc28b1ee9f10
iAtomPlaza <stevecoulter99@gmail.com>
2021-06-30 12:42:08 -0700

    modify component.go

--- component.go
+++ component.go
@@ -1,5 +1,27 @@
 package gerbil
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type Component interface {
 	Render() string
+}
+
+func IsComponent(c interface{}) bool {
+
+	iface := reflect.TypeOf((*Component)(nil)).Elem()
+	component := reflect.ValueOf(c)
+	if component.Kind() != reflect.Ptr {
+		fmt.Printf("'%s' must be a pointer to a struct that implements the '*gerbil.Component' interface\n", component.Type())
+		return false
+	}
+
+	if !component.Type().Implements(iface) {
+		fmt.Printf("error: component %v does not implement %v\n", component.Type(), iface)
+		return false
+	}
+
+	return true
 }
